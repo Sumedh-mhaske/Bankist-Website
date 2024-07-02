4f4698081c9cfa7fb64cadfbd5c957a6908044f8
Sumedh Mhaske <sumedhmhaske310503@gmail.com>
2024-07-02 20:45:48 +0530

    Tabbed components

--- script.js
+++ script.js
@@ -49,3 +49,24 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Tabbed components
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Activate content area
+  clicked.classList.add("operations__tab--active");
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
